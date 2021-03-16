f75a2807395a4f5f56c216a92907629fb8299ac0
cobsccomp192p-001 <77620302+cobsccomp192p-001@users.noreply.github.com>
2021-03-17 00:23:18 +0530

    validations

--- NIBM-FoodOrdering/Controllers/LoginViewController.swift
+++ NIBM-FoodOrdering/Controllers/LoginViewController.swift
@@ -10,21 +10,49 @@ class LoginViewController: UIViewController {
 
     @IBAction func loginPressed(_ sender: UIButton) {
         
-        if let email = emailTextfield.text, let password = passwordTextfield.text
+        if emailTextfield.text!.isEmpty || passwordTextfield.text!.isEmpty
         {
-            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            AlertMesg(msg: "Enter all fields")
+            
+        }
+        else
+        {
+            if let email = emailTextfield.text, let password = passwordTextfield.text
+            {
+                Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
               
                 if let e=error
                 {
-                    print(e)
+                    self.ErrAlertMesg(error: e.localizedDescription)
                 }
                 else
                 {
                     self.performSegue(withIdentifier: K.loginSegue, sender: self)
                 }
+                    
+                }
             }
         }
         
+        
+    }
+    
+    func ErrAlertMesg(error:String)
+    {
+        let dialogMessage = UIAlertController(title: "Alert", message: error, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "Try Again", style: .default, handler: nil)
+        dialogMessage.addAction(ok)
+         
+        self.present(dialogMessage, animated: true, completion: nil)
+    }
+    
+    func AlertMesg(msg:String)
+    {
+        let dialogMessage = UIAlertController(title: "Alert", message: msg, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        dialogMessage.addAction(ok)
+         
+        self.present(dialogMessage, animated: true, completion: nil)
     }
     
 }
